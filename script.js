ea2ed740ca7372145939d906c5fcbbbe3d7eb37a
ymeah <112223055+ymeah@users.noreply.github.com>
2023-06-18 12:36:10 -0500

    made astroides

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const statusDiv = document.getElementById("status");
 const canvas = document.getElementById("game-area");
 const ctx = canvas.getContext("2d");
 
+let prjs=[];
+
 // Set the canvas size to 400x400
 canvas.width = 400;
 canvas.height = 400;
@@ -10,11 +12,11 @@ canvas.height = 400;
 const gravity = 0.01;
 const sideEngineThrust = 0.01;
 const mainEngineThrust = 0.03;
-
+const lzBluffer = 5;
 const ship = {
   color: "blue",
   // height, width
-  w: 800,
+  w: 10,
   h: 22,
   // position
   x: 0,
@@ -29,6 +31,23 @@ const ship = {
   landed: false,
 };
 
+const platform = {
+  color: 'black',
+  w: 20,
+  h: 5,
+  x: 190,
+  y: 345,
+  top: 350,
+  bottom: 345,
+  left: 190,
+  right: 210,
+}
+
+function drawPlatform() {
+  ctx.fillStyle = platform.color;
+  ctx.fillRect(platform.x, platform.y, platform.w, platform.h);
+}
+
 function initShip() {
   // position
   ship.x = 150 + Math.random() * 100;
@@ -41,6 +60,21 @@ function initShip() {
   ship.rightEngine = false;
   ship.crashed = false;
   ship.landed = false;
+}
+
+function initPrjs() {
+  for (let i = 0; i < 10; i++) {
+    let prj = {
+      x: Math.floor(Math.random() * 400),
+      y: 0,
+      dx: 1 - (Math.random() * 2),
+      dy: Math.random(),
+      h: 4,
+      w: 4,
+      color: 'brown'
+    };
+    prjs.push(prj);
+  }
 }
 
 function drawTriangle(a, b, c, fillStyle) {
@@ -100,6 +134,12 @@ ship.dy += gravity;
 if(ship.mainEngine){
   ship.dy -= mainEngineThrust;
 }
+if(ship.rightEngine){
+  ship.dx -= sideEngineThrust;
+}
+if(ship.leftEngine){
+  ship.dx += sideEngineThrust;
+}
 ship.y += ship.dy;
 ship.x += ship.dx;
 
@@ -110,19 +150,61 @@ ship.x += ship.dx;
   // TODO: update the position - how does dx, dy affect x, y?
 }
 
+function drawPrjs() {
+  for (let i= 0; i < prjs.length; i++) {
+    let prj = prjs[i];
+    ctx.fillStyle = prj.color;
+    ctx.fillRect(prj.x, prj.y, prj.w, prj.h);
+  }
+}
+function updatePjrs() {
+  for (let i = 0; i < prjs.length; i++) {
+    let prj = prjs[i];
+    prj.dy += gravity
+    prj.y += prj.dy;
+    prj.x += prj.dx;
+  }
+}
 function checkCollision() {
   const top = ship.y - ship.h / 2;
   const bottom = ship.y + ship.h / 2;
   const left = ship.x - ship.w / 2;
   const right = ship.x + ship.w / 2;
   // TODO: check that ship flew out of bounds. If so, set ship.crashed = true
+  if(top < 0|| bottom > canvas.height|| right > canvas.width|| left < 0){
+    ship.crashed = true;
+    return;
+  }
 
+  const isNotOverlapingPlatform =
+    bottom < platform.top ||
+    top > platform.bottom ||
+    left > platform.right ||
+    right < platform.left;
+   if (!isNotOverlapingPlatform) {
+    ship.crashed = true;
+    return;
+   }
+
+   if ( 
+    ship.dx < 0.2 &&
+    ship.dy < 0.2 &&
+    left > platform.left &&
+    right < platform.right &&
+    bottom < platform.top &&
+    platform.top - bottom < lzBluffer
+    ) {
+      ship.landed = true;
+      return;
+
+    }
   // TODO: check if ship landed. If so, set ship.landed = true
   // - What conditions have to be true for a soft landing?
 }
 
 function gameLoop() {
   updateShip();
+  updatePjrs();
 
   checkCollision();
   if (ship.crashed) {
@@ -135,6 +217,8 @@ function gameLoop() {
     // Clear entire screen
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawShip();
+    drawPrjs();
+    drawPlatform();
     requestAnimationFrame(gameLoop);
   }
 }
@@ -181,6 +265,7 @@ function start() {
   statusDiv.innerHTML = "";
   initShip();
 
+  initPrjs();
   document.addEventListener("keyup", keyLetGo);
   document.addEventListener("keydown", keyPressed);
   requestAnimationFrame(gameLoop);
